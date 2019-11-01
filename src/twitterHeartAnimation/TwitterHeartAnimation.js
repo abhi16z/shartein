43b7436db3c16058473ece11bb5278eefc922f7f
MMT8048 <abhishek.kumar4@go-mmt.com>
2019-11-01 21:01:19 +0530

    little heart functionality

--- src/twitterHeartAnimation/TwitterHeartAnimation.js
+++ src/twitterHeartAnimation/TwitterHeartAnimation.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './twitterHeartAnimation.scss';
 
 export default function TwitterHeartAnimation() {
-  return <button className="heart" />
+  const [isSelected, setIsSelected] = useState(false);
+  return <button className={isSelected ? 'heart' : 'heartless'} onClick={() => setIsSelected(!isSelected)} />
 }
